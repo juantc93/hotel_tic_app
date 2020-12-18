8db624e19d0f3a5de50ce9fffb6c3787c822456f
juantc93 <52570565+juantc93@users.noreply.github.com>
2020-12-18 00:08:09 -0500

    nuevo commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,32 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
+    <div class ="componentes">
+    <h1>Hotel TIC (No es mucho pero es trabajo honesto ( ͡° ͜ʖ ͡°) ) </h1>
+    <h2>App de un solo uso, cumple con lo requerido en el sprint (Botón con método PUT y Botón con método GET)</h2>
+    <button v-on:click="getOcupacion">Ocupación</button>
+    <button v-on:click="CrearReserva">Crear Reserva</button>
+    </div>
+    <div class="main-component">
+      <router-view></router-view>
+    </div>
   </div>
-</template>
+ </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+export default{
+  name:"App",
+  components:{},
+  methods:{
+    getOcupacion:function(){
+      this.$router.push({name:"ocupacion"})
+      },
+      CrearReserva:function(){
+        this.$router.push({name:"crear-reserva"})
+      }
+  },
+  
+} 
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
